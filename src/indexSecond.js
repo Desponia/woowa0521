4678fe5530c8aef102b0be4855483ef92380277b
Jihye <coalcellar@gmail.com>
2019-05-21 20:13:23 +0900

    set yield as generator with iterable object, then do next()

--- src/indexSecond.js
+++ src/indexSecond.js
@@ -2,6 +2,8 @@ function* foo() {
   // yield 10;
   // yield 20;
   // yield 30;
+  yield* [1, 2, 3];
+
   let num = 0;
   while (true) {
     const r = yield ++num;
@@ -12,6 +14,9 @@ function* foo() {
 
 const result = foo();
 
+console.log(result.next());
+console.log(result.next());
+console.log(result.next());
 console.log(result.next());
 console.log(result.next("A"));
 console.log(result.next("B"));
